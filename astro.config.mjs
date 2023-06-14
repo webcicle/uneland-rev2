288493058bb4c4614e359bbd93b28557d6ba2655
webcicle <89933280+webcicle@users.noreply.github.com>
2023-06-14 10:03:38 +0200

    Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		tailwind({
-			config: { applyBaseStyles: false },
+			config: {
+				applyBaseStyles: false,
+			},
 		}),
 		react(),
 	],
 	output: 'server',
+	adapter: vercel(),
 });
-
-export const prerender = false;
